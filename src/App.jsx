c1e11db93fad10cf2c8aecd10766540d6e7666a4
SokeBiz <oladsodeeq148@gmail.com>
2023-01-26 11:31:38 +0100

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import Videoplayer from './Videoplayer'
 
-function App()
+function App() {
 
   return (
     <>
       <Videoplayer />
     </>
   )
-
+}
 
 export default App
